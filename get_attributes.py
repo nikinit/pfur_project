2ba2301a3a9946be6a86a2e2ac298c16994668af
Nikita <kni0805@yandex.ru>
2021-05-03 23:44:05 +0300

    separate function for getting planes

--- get_attributes.py
+++ get_attributes.py
@@ -1,16 +1,28 @@
 import xml.etree.ElementTree as et
-def get_products(request, file = 'data.xml'):
-    attributes = []
-    request = request.split(' ')
+
+def get_planes(file = 'data.xml'):
+    planes = []
     root = et.parse(file).getroot()
     product_table = root[2][0]
     for product in product_table:
         plane = {}
         for assign in product:
-            for i in range(len(request)):
-                if (assign.get('applicPropertyIdent') == request[i]):
-                    plane[assign.get('applicPropertyIdent')] = assign.get('applicPropertyValues')
-        attributes.append(plane)
+            plane[assign.get('applicPropertyIdent')] = assign.get('applicPropertyValues')
+        planes.append(plane)
+    return planes
+
+def get_attributes(request, planes):
+    attributes = []
+    for plane in planes:
+        plane_attributes = {}
+        for attribute in request:
+            try:
+                plane_attributes[attribute] = plane[attribute]
+            except KeyError:
+                continue
+        attributes.append(plane_attributes)
     return attributes
-request = input('Enter attributes you want to get, separated by space button:')
-print(get_products((request)))
+
+planes = get_planes()
+request = input('Enter attributes you want to get, separated by space button:').split(' ')
+print(get_attributes(request, planes))
