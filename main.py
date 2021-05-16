a244160e786ac502c78d4fe795b659750b98c4b1
Nikita <kni0805@yandex.ru>
2021-05-16 13:32:41 +0300

    XML updates when you delete plane

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ tree = et.parse('data.xml')
 root = tree.getroot()
 product_table = root[2][0]
 planes = get_attributes.get_planes(product_table, registry_numbers, planes)
-request = input('Do you want to "add" or "change": ')
+request = input('Do you want to "add" or "change" or "delete": ')
 print(request_handler.handle_request(request, registry_numbers, planes, tree, product_table))
