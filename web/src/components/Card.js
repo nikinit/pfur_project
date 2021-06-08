eddc69f2a451b4a9f67c88c9cceedacf13c88384
Nikita <kni0805@yandex.ru>
2021-06-08 19:52:37 +0300

    only one file request on loading

--- web/src/components/Card.js
+++ web/src/components/Card.js
@@ -1,62 +1,69 @@
 import React from "react";
-import data from "../data/data.xml";
-// import CardPopup from "./CardPopup";
-let loadedPlanes = 0;
 export default class Card extends React.Component {
     constructor(props) {
         super(props);
+        this.showAllButton = this.showAllButton.bind(this);
+        this.editButton = this.editButton.bind(this);
         this.state = {
             error: null,
-            onLoaded: false,
+            infoIsOpen: false,
             characteristics: {},
             logo: "https://4.bp.blogspot.com/-VCZ54hux6aA/VzxAQaAesPI/AAAAAAAAEAY/d9pFAjKYEIsR3_oWJfYR2pwRU9Pd6dW5gCLcB/s1600/68652_722142921137870_729498003_n.png"
             // TODO: fix logo problem so it goes with characteristics
         }
     }
 
-    componentDidMount() {
-        fetch(data)
-        .then(resp => resp.text())
-        .then(dataStr => new DOMParser()
-        .parseFromString(dataStr, 'text/xml')
-        .getElementsByTagName('product'))
-        .then((result) => {
-            this.setState({
-                onLoaded: true
-            })
-            let newCharacteristics = {};
-            let propsAmount = result[loadedPlanes].children.length;
-            for (let i = 0; i < propsAmount; i++) {
-                let propName = result[loadedPlanes].children[i].attributes['applicPropertyIdent'].nodeValue;
-                let propValue = result[loadedPlanes].children[i].attributes['applicPropertyValues'].nodeValue;
-                newCharacteristics[propName] = propValue;
-                this.setState({
-                    characteristics : newCharacteristics
-                })
-            }
-            loadedPlanes++;
+    showAllButton(event) {
+        this.setState({
+            infoIsOpen: !this.state.infoIsOpen
         })
     }
 
+    editButton(event) {
+        console.log(event)
+    }
+    componentDidUpdate(prevProps, prevState) {
+        // console.log(prevProps,prevState)
+        console.log(this.state.characteristics)
+        if (this.props.characteristics !== undefined && prevProps.characteristics === undefined) {
+            this.setState({
+                characteristics: this.props.characteristics
+        });
+        // debugger;
+        }
+    }
     render() {
+        const {infoIsOpen, characteristics} = this.state;
         return (
-            
             <div className="cards__card-main">
                 <div className="cards__card-logo">
                     <img src={this.state.logo} alt="logo" className=""></img>
                 </div>
                 <div className="cards__card-info">
                     <div className="cards__card-general">
-                        <h3 className="cards__card-prop cards__card-prop_name">{this.state.characteristics.model + " " + this.state.characteristics.minorModel + " " + this.state.characteristics.subModel}</h3>
-                        <h4 className="cards__card-prop">{this.state.characteristics.registryNbr}</h4>
+                        <h3 className="cards__card-prop cards__card-prop_name">{characteristics.model + " " + characteristics.minorModel + " " + characteristics.subModel}</h3>
+                        <h4 className="cards__card-prop">{characteristics.registryNbr}</h4>
                     </div>
                     <div className="cards__card-buttons">
-                        <button className="cards__button cards__card-button">Показать всё</button>
-                        <button className="cards__button cards__card-button cards__card-button_change">Изменить</button>
+                        <button className="cards__button cards__card-button" onClick={this.showAllButton}>Показать всё</button>
+                        <button className="cards__button cards__card-button cards__card-button_change" onClick={this.editButton}>Изменить</button>
                     </div>
                 </div>
+                <div className="cards__card-popup" style={{
+                display: !infoIsOpen ? "none" : "flex",
+                visibility: !infoIsOpen ? "hidden" : "visible"
+                }}>
+                    <h4 className="cards__card-prop">Model : {characteristics.model}</h4>
+                    <h4 className="cards__card-prop">minorModel : {characteristics.minorModel}</h4>
+                    <h4 className="cards__card-prop">subModel : {characteristics.subModel}</h4>
+                    <h4 className="cards__card-prop">registryNbr : {characteristics.registryNbr}</h4>
+                    <h4 className="cards__card-prop">lineNbr : {characteristics.lineNbr}</h4>
+                    <h4 className="cards__card-prop">operator : {characteristics.operator}</h4>
+                    <h4 className="cards__card-prop">electricalDifferences : {characteristics.electricalDifferences}</h4>
+                    <h4 className="cards__card-prop">airCondDifferences : {characteristics.airCondDifferences}</h4>
+                    <h4 className="cards__card-prop">modification : {characteristics.modification}</h4>
+                </div>
             </div>
-            
         )
     }
 }
